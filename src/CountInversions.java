916b7fdd1704835e164b5da4b6b30e6318939d55
Dianwen Li <dianwen@gmail.com>
2015-04-28 15:41:56 -0400

    Added inversion counting and tests

---
+++ src/CountInversions.java
@@ -0,0 +1,98 @@
+/**
+ * Created by dianwen on 4/28/15.
+ */
+public class CountInversions {
+    public static int countInversionsAndMergeSort(int[] array) {
+        if(array.length == 1) {
+            return 0;
+        }
+        int[] leftHalf = new int[array.length/2];
+        int[] rightHalf = new int[array.length - array.length/2]; // Account for odd length arrays
+        for(int i = 0 ; i < array.length/2; i++) {
+            leftHalf[i] = array[i];
+        }
+        for(int i = array.length/2; i < array.length; i++) {
+            rightHalf[i - array.length/2] = array[i];
+        }
+        int leftInversions = countInversionsAndMergeSort(leftHalf);
+        int rightInversions = countInversionsAndMergeSort(rightHalf);
+        int splitInversions = countSplitInversionsAndMerge(leftHalf, rightHalf, array);
+        return leftInversions + rightInversions + splitInversions;
+    }
+
+//    private static int countSplitInversionsAndMerge(int[] leftHalf, int[] rightHalf, int[] result) {
+//        int i = 0;
+//        int j = 0;
+//        int totalSize = leftHalf.length + rightHalf.length;
+//        result = new int[totalSize];
+//        int inversions = 0;
+//        for(int k = 0; k < totalSize; k++) {
+//            int leftValue, rightValue;
+//            if(i >= leftHalf.length) {
+//                leftValue = Integer.MAX_VALUE;
+//            }
+//            else {
+//                leftValue = leftHalf[i];
+//            }
+//            if(j >= rightHalf.length) {
+//                rightValue = Integer.MAX_VALUE;
+//            }
+//            else {
+//                rightValue = rightHalf[j];
+//            }
+//            if(leftValue < rightValue) {
+//                result[k] = leftValue;
+//                i++;
+//            }
+//            else {
+//                result[k] = rightValue;
+//                j++;
+//                if(i < leftHalf.length) { // Found inversion, copying from the right half before the left half is exhausted
+//                    inversions += leftHalf.length - i; // The number of inversions caused is the remaining length of the left array
+//                }
+//            }
+//        }
+//        return inversions;
+//    }
+
+    private static int countSplitInversionsAndMerge(int[] leftHalf, int[] rightHalf, int[] result) {
+        int i = 0;
+        int j = 0;
+        int inversions = 0;
+        int totalSize = leftHalf.length + rightHalf.length;
+        for(int k = 0; k < totalSize; k++) {
+            if(i < leftHalf.length && j < rightHalf.length) {
+                if(leftHalf[i] <= rightHalf[j]) {
+                    result[k] = leftHalf[i];
+                    i++;
+                }
+                else {
+                    result[k] = rightHalf[j];
+                    j++;
+                    inversions += leftHalf.length - i;
+                }
+            }
+            else if(i >= leftHalf.length) {
+                result[k] = rightHalf[j];
+                j++;
+            }
+            else {
+                result[k] = leftHalf[i];
+                i++;
+            }
+        }
+        return inversions;
+    }
+
+    public static int bruteForceCountInversions(int[] array) {
+        int inversions = 0;
+        for(int i = 0; i < array.length; i++) {
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[j] < array[i]) {
+                    inversions++;
+                }
+            }
+        }
+        return inversions;
+    }
+}
---
+++ src/CountInversionsTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by dianwen on 4/27/15.
+ */
+public class CountInversionsTest {
+    @Test
+    public void testCorrectlyCountInversions() {
+        int[] array = {1, 3, 5, 2, 4, 6};
+        assertEquals((long) 3, (long) CountInversions.countInversionsAndMergeSort(array));
+    }
+
+    @Test
+    public void testMergeSortMatchesBruteForceCount() {
+        int[] randomArray = createRandomArray(32, 100, 0);
+        assertEquals((long) CountInversions.bruteForceCountInversions(randomArray), (long) CountInversions.countInversionsAndMergeSort(randomArray));
+    }
+
+    public static int[] createRandomArray(int arraySize, int maxValue, int minValue) {
+        Random rand = new Random();
+        int[] array = new int[arraySize];
+        for(int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(maxValue - minValue) + minValue;
+        }
+        return array;
+    }
+}
