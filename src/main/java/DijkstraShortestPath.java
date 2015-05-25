18599474439232e6a49ee701a57604321ed79e35
Dianwen Li <dianwen@gmail.com>
2015-05-25 15:32:43 -0400

    Added Dijkstra implementation and and modifications to Graph object to support

--- src/main/java/DijkstraShortestPath.java
+++ src/main/java/DijkstraShortestPath.java
@@ -1,8 +1,72 @@
 package main.java;
 
+import main.resources.Graph;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * Created by dianwen on 5/24/15.
  */
 public class DijkstraShortestPath {
+    public static void main(String[] args) {
+        Integer[][] adjacencyMatrix = {{1, 2}, {2, 3}, {3}, {}};
+        Integer[][] edgeWeights = {{1, 4}, {2, 6}, {3}, {}};
+        Graph g = new Graph(adjacencyMatrix, edgeWeights);
+        System.out.println(Arrays.toString(findShortestPath(g, 0)));
+    }
 
+    public static int[] findShortestPath(Graph g, int startVertex) {
+        int[] shortestPaths = new int[g.getVerticiesCount()];
+        for(int i = 0; i < shortestPaths.length; i++) {
+            shortestPaths[i] = Integer.MAX_VALUE;
+        }
+        shortestPaths[startVertex] = 0;
+        Queue<Node> q = new PriorityQueue<>();
+        q.add(new Node(startVertex, 0));
+
+        while(!q.isEmpty()) {
+            Node currentNode = q.remove();
+            Integer[] neighbors = g.getNeighboringVerticies(currentNode.vertexId);
+            for(int neighbor : neighbors) {
+                int newNeighborDistance = currentNode.minDistance + g.getEdgeWeight(currentNode.vertexId, neighbor);
+                if(newNeighborDistance < shortestPaths[neighbor]) {
+                    shortestPaths[neighbor] = newNeighborDistance;
+                    q.remove(new Node(neighbor));
+                    q.add(new Node(neighbor, newNeighborDistance));
+                }
+            }
+
+        }
+        return shortestPaths;
+    }
+
+    public static class Node implements Comparable<Node>{
+        int minDistance;
+        int vertexId;
+
+        public Node(int vertexId, int minDistance) {
+            this.vertexId = vertexId;
+            this.minDistance = minDistance;
+        }
+
+        public Node(int vertexId) {
+            this(vertexId, Integer.MAX_VALUE);
+        }
+
+        @Override
+        public int compareTo(Node that) {
+            return this.minDistance - that.minDistance;
+        }
+
+        @Override
+        public boolean equals(Object that) {
+            if(that instanceof Node) {
+                Node castedThat = (Node) that;
+                return this.vertexId == castedThat.vertexId;
+            }
+            return false;
+        }
+    }
 }
--- src/main/resources/Graph.java
+++ src/main/resources/Graph.java
@@ -1,24 +1,40 @@
 package main.resources;
 
+import java.util.Arrays;
+
 /**
  * Created by dianwen on 5/18/15.
  */
 public class Graph {
     Integer[][] adjacencyMatrix;
+    Integer[][] weights;
     Integer edges;
     Integer verticies;
 
     public Graph(Integer[][] adjacencyMatrix) {
+        this(adjacencyMatrix, new Integer[adjacencyMatrix.length][]);
+    }
+
+    public Graph(Integer[][] adjacencyMatrix, Integer[][] weights) {
         this.adjacencyMatrix = adjacencyMatrix;
         verticies = adjacencyMatrix.length;
         edges = 0;
         for(int i = 0; i < adjacencyMatrix.length; i++) {
             edges += adjacencyMatrix[i].length;
+            if(weights[i] == null) {
+                weights[i] = new Integer[adjacencyMatrix[i].length];
+            }
         }
+        this.weights = weights;
     }
 
     public Integer[] getNeighboringVerticies(int vertex) {
         return adjacencyMatrix[vertex];
+    }
+
+    public Integer getEdgeWeight(int vertexA, int vertexB) {
+        int indexB = Arrays.asList(adjacencyMatrix[vertexA]).indexOf(vertexB);
+        return weights[vertexA][indexB];
     }
 
     public Integer getEdgesCount() {
